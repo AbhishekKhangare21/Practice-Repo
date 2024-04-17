a24adb5a5b24a8de253aa9f3fc22d97db800a79d
Abhishek Khangare <abhi@gmail.com>
2024-04-17 15:32:04 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,63 +1,10 @@
-// function x() {
-//   var a = 7;
-//   function y() {
-//     console.log(a);
-//   }
-//   y();
-// }
-// x();
+function x() {
+  for (var i = 1; i <= 5; i++) {
+    setTimeout(function () {
+      console.log(i);
+    }, i * 1000);
+  }
+  console.log("Namaste JavaScript");
+}
 
-// function x(y) {
-//   y();
-// }
-// x(function y() {
-//   var a = 7;
-//   console.log(a);
-// });
-
-// Returning function
-
-// function x() {
-//   var a = 7;
-//   function y() {
-//     console.log(a);
-//   }
-//   return y;
-// }
-// var z = x();
-// console.log(z);
-// z();
-
-// function x() {
-//   var a = 7;
-//   return function y() {
-//     console.log(a);
-//   };
-// }
-// var z = x();
-// z();
-
-// function x() {
-//   var a = 7;
-//   function y() {
-//     console.log(a);
-//   }
-//   a = 100;
-//   return y;
-// }
-// var z = x();
-// console.log(z);
-// z();
-
-// function z() {
-//   var b = 900;
-//   function x() {
-//     var a = 7;
-//     function y() {
-//       console.log(a, b);
-//     }
-//     y();
-//   }
-//   x();
-// }
-// z();
+x();
