6d37f96178b6d90f84fb4542d6a9ea1d5169175c
Abhishek Khangare <abhi@gmail.com>
2024-04-17 10:31:31 +0530

    Closure

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
-const b = 100;
-{
-  let b = 10;
-  console.log(b);
-}
+// function x() {
+//   var a = 7;
+//   function y() {
+//     console.log(a);
+//   }
+//   y();
+// }
+// x();
 
-console.log(b);
+// function x(y) {
+//   y();
+// }
+// x(function y() {
+//   var a = 7;
+//   console.log(a);
+// });
+
+// Returning function
+
+// function x() {
+//   var a = 7;
+//   function y() {
+//     console.log(a);
+//   }
+//   return y;
+// }
+// var z = x();
+// console.log(z);
+// z();
+
+// function x() {
+//   var a = 7;
+//   return function y() {
+//     console.log(a);
+//   };
+// }
+// var z = x();
+// z();
+
+// function x() {
+//   var a = 7;
+//   function y() {
+//     console.log(a);
+//   }
+//   a = 100;
+//   return y;
+// }
+// var z = x();
+// console.log(z);
+// z();
+
+// function z() {
+//   var b = 900;
+//   function x() {
+//     var a = 7;
+//     function y() {
+//       console.log(a, b);
+//     }
+//     y();
+//   }
+//   x();
+// }
+// z();
