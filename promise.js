5070d22a4faa89850f28ceedddb0ea8c7cd4323b
Abhishek Khangare <abhi@gmail.com>
2024-04-21 16:37:45 +0530

    promise.js

--- promise.js
+++ promise.js
@@ -3,9 +3,23 @@ const cart = ["shoes", "pants", "kurta"];
 createOrder(cart)
   .then(function (orderId) {
     console.log(orderId);
-    //   proceedToPayment(orderId);
+    return orderId;
   })
-  .catch((err) => console.log(err));
+  .catch(function (err) {
+    console.log(err.message);
+  })
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  })
+  .then(function (paymentInfo) {
+    console.log(paymentInfo);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  })
+  .then(function (orderId) {
+    console.log("No matter what happens, I will definietely be called.");
+  });
 
 function createOrder(cart) {
   const pr = new Promise(function (resolve, reject) {
