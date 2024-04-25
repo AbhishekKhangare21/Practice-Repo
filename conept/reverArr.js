dde38e3af4a7319a7be9de4ca31e3db1ab991599
Abhishek Khangare <abhi@gmail.com>
2024-04-25 16:54:14 +0530

    Add reverArr.js

---
+++ conept/reverArr.js
@@ -0,0 +1,12 @@
+var a = [10, 34, 56, 78];
+
+// console.log(a.length);
+// console.log(a.reverse());
+
+// for (var i = 5; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// for (var i = a.length - 1; i >= 0; i--) {
+//   console.log(a[i]);
+// }
