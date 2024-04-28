163a56c961326d1fae07735cbe2684dab375aa46
Abhishek Khangare <abhi@gmail.com>
2024-04-28 16:55:10 +0530

    14.js

---
+++ JCC/14.js
@@ -0,0 +1,27 @@
+// "use strict";
+
+// const obj = {
+//   name: {},
+// };
+
+// Object.freeze(obj);
+// obj.name.firstName = "Test2";
+// console.log(obj.name.firstName);
+
+// console.log(obj);
+
+// 2nd Example
+
+// const obj1 = {
+//   subject: "javascript",
+//   rating: 9,
+// };
+
+// obj1.subject = "Angular";
+// delete obj1.rating;
+
+// console.log(obj1);
+
+// Object.freeze(obj1);
+
+// obj1.subject = "Typescript";
