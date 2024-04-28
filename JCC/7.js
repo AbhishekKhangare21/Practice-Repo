6aadde0a6f70a06bec81d8e98c45b6dc1ee1b16b
Abhishek Khangare <abhi@gmail.com>
2024-04-28 15:36:52 +0530

    7.js

---
+++ JCC/7.js
@@ -0,0 +1,11 @@
+function print(x) {
+  if (x <= 1) {
+    print(x + 1);
+    console.log("x in if block is " + x);
+  } else if (x >= 1000000000000) {
+    console.log("x in else if block is" + x);
+  }
+  print(x + 1);
+}
+
+print(0);
