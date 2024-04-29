5130361517aa7d46d80fcf8f92523687e4ed1f81
Abhishek Khangare <abhi@gmail.com>
2024-04-29 17:23:13 +0530

    Add 31.js

---
+++ JCC/31.js
@@ -0,0 +1,11 @@
+setTimeout(() => {
+  console.log("String1");
+}, "1000");
+
+setTimeout(() => {
+  console.log("String2");
+}, "1 second");
+
+setTimeout(() => {
+  console.log("Number");
+}, 1000);
