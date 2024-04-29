60b4fdf585ecfa4a558c08512e98c0bcf079a894
Abhishek Khangare <abhi@gmail.com>
2024-04-29 15:09:58 +0530

    19.js

---
+++ JCC/19.js
@@ -0,0 +1,9 @@
+//Question: Find Code Output (With reason)
+
+let str1 = "Learn";
+let str2 = "Javascript";
+
+console.log(str1.padEnd(6) + str2.padEnd(1));
+
+let result = str1.padEnd(6);
+console.log(result);
