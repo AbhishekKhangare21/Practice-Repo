5d713e118f0d8bc6151cf46c7c82aa766832bced
Abhishek Khangare <abhi@gmail.com>
2024-04-29 15:00:29 +0530

    18.js

---
+++ JCC/18.js
@@ -0,0 +1,10 @@
+let num1 = Math.sqrt(-9);
+let num2 = Math.sqrt(9);
+
+if (num1 + num2 >= 0) {
+  console.log("Positive Value");
+} else if (num1 + num2 < 0) {
+  console.log("Negative Value");
+} else {
+  console.log("Not number value");
+}
