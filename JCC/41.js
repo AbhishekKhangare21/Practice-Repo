7b066654e533d8d14bc06d88384ea752c462128f
Abhishek Khangare <abhi@gmail.com>
2024-04-29 18:31:34 +0530

    Add 41.js

---
+++ JCC/41.js
@@ -0,0 +1,13 @@
+async function async1() {
+  setTimeout(() => {
+    console.log("async1");
+  }, 0);
+}
+
+async function async2() {
+  console.log("start");
+  await async1();
+  console.log("end");
+}
+
+async2();
