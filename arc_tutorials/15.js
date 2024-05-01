547cec3ed07a9eccdfa00f2df4cad84649b5de52
Abhishek Khangare <abhi@gmail.com>
2024-05-01 17:26:00 +0530

    Add 15.js

---
+++ arc_tutorials/15.js
@@ -0,0 +1,18 @@
+// const elementsToJoin = [
+//   "WElcome",
+//   "to",
+//   "ARC Tutorials",
+//   "Javascript",
+//   "Tutorials",
+// ];
+
+//1st method
+// console.log(elementsToJoin.join(" "));
+
+//2nd method
+// let str = " ";
+// for (let i = 0; i < elementsToJoin.length; i++) {
+//   str = str + elementsToJoin[i] + " ";
+// }
+
+// console.log(str);
