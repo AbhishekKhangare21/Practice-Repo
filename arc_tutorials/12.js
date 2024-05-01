c70a6733290c59f23096123577cc7075e38bc32b
Abhishek Khangare <abhi@gmail.com>
2024-05-01 10:29:14 +0530

    Add 12.js

---
+++ arc_tutorials/12.js
@@ -0,0 +1,26 @@
+// How to find minimum value in a numbered array?
+
+// Using for loop
+
+const numberArr = [5, 45, 54, 63, 87, 72, 98, 12, 10];
+
+// const minimumVal = (arr) => {
+//   let min = Infinity;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (min > arr[i]) {
+//       min = arr[i];
+//     }
+//   }
+//   return min;
+// };
+
+// console.log(minimumVal(numberArr));
+
+// Using Reduce Method
+
+// const reduce = numberArr.reduce((acc, curr) => {
+//   return acc > curr ? curr : acc;
+// });
+
+// console.log(reduce);
