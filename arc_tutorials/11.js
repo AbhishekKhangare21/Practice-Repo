7a01ca9b12f41b7ede6d27b69f5ed9863d46fbf2
Abhishek Khangare <abhi@gmail.com>
2024-05-01 10:02:48 +0530

    Add 11.js

---
+++ arc_tutorials/11.js
@@ -0,0 +1,24 @@
+// How to find maximum value in a numbered array?
+
+// using for loop (old method)
+// const nummbers = [4, 21, 4, 6, 59, 23];
+
+// const maximumNum = (num) => {
+//   let max = 0;
+
+//   for (let i = 0; i < num.length; i++) {
+//     if (max < num[i]) {
+//       max = num[i];
+//     }
+//   }
+
+//   return max;
+// };
+
+// console.log(maximumNum(nummbers));
+
+// const numberArr = [5, 45, 54, 63, 87, 72, 98, 12, 10];
+
+// const output = numberArr.reduce((acc, curr) => (acc > curr ? acc : curr));
+
+// console.log(output);
