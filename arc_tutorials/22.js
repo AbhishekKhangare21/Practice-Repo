a9db05590fee00641876ed4d96b8236e570e9b65
Abhishek Khangare <abhi@gmail.com>
2024-05-01 22:38:35 +0530

    Add 22.js

---
+++ arc_tutorials/22.js
@@ -0,0 +1,15 @@
+//How to empty an array?
+
+let freinds = ["Aniket", "Pranit"];
+
+// freinds = [];
+
+// freinds.length = 0;
+
+// while (freinds.length > 0) {
+//   freinds.pop();
+// }
+
+// freinds.splice(0, freinds.length);
+
+console.log(freinds);
