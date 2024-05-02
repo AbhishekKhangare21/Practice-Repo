ee857816b9c7aaf8eeba702c35f215a20f13d144
Abhishek Khangare <abhi@gmail.com>
2024-05-02 14:49:16 +0530

    Add 25.js

---
+++ arc_tutorials/25.js
@@ -0,0 +1,10 @@
+// // Write a function to check if a given string is palindrome or not?
+
+// function checkPalindrome(str) {
+//   let str1 = str.replace(/\w/g, "").toLowerCase();
+//   return str == str1.split("").reverse().join();
+// }
+
+// console.log(checkPalindrome("madam"));
+
+//this is wrong
