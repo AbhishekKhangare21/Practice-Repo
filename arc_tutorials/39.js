0581cdfa89f7d41ad4e0c5fab8dc182d8be2911e
Abhishek Khangare <abhi@gmail.com>
2024-05-02 18:27:42 +0530

    Add 39.js

---
+++ arc_tutorials/39.js
@@ -0,0 +1,9 @@
+//Write a method which takes x number of parameters?
+
+function getParams(...args) {
+  console.log(...args);
+}
+
+getParams(1);
+getParams(3, 4, 5);
+getParams(3, 4, 5, 6, 7, 8, 9);
