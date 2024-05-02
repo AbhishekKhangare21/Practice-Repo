b68485bd1687d687a3b58b6e64fd117a811bbbe2
Abhishek Khangare <abhi@gmail.com>
2024-05-02 14:26:18 +0530

    Add 24.js

---
+++ arc_tutorials/24.js
@@ -0,0 +1,7 @@
+// How do you sort and reverse an array without changing the original array?
+
+let students = ["k", "a", "b", "c", "d", "e", "f"];
+
+const newStudentArr = students.slice().reverse();
+
+console.log(newStudentArr);
