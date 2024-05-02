a2f428a7f5b305451de2a50283c28dfec66bd64e
Abhishek Khangare <abhi@gmail.com>
2024-05-02 19:06:07 +0530

    Add 41.js

---
+++ arc_tutorials/41.js
@@ -0,0 +1,11 @@
+// How can you return a character from a string at a specific index?
+
+const str = "Welcome Abhishek Khangare";
+
+const letter = str.charAt(0).toUpperCase();
+
+const substr = str.substring(1).toLowerCase();
+
+console.log(letter, substr);
+
+console.log(letter);
